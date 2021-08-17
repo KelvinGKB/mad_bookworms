8fd7c67caca4ba87f6f9c5d3eaa7de16b26ddeea
Tan Chia Ter <chiater0311@gmail.com>
2021-08-17 14:06:37 +0800

    ChiaTer

--- app/src/main/java/com/mad/mad_bookworms/ExploreFragment.kt
+++
@@ -1,7 +0,0 @@
-
-import androidx.fragment.app.Fragment
-import com.mad.mad_bookworms.R
-
-class ExploreFragment:Fragment(R.layout.fragment_explore) {
-
-}
--- app/src/main/java/com/mad/mad_bookworms/MainActivity.kt
+++ app/src/main/java/com/mad/mad_bookworms/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.mad.mad_bookworms
 
 import CartFragment
-import ExploreFragment
 import ProfileFragment
 import RedeemFragment
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mad.mad_bookworms.Customer.Home.ExploreFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val exploreFragment=ExploreFragment()
+        val exploreFragment= ExploreFragment()
         val cartFragment=CartFragment()
         val redeemFragment=RedeemFragment()
         val profileFragment=ProfileFragment()
